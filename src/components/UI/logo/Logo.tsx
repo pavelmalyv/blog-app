cec4fb172f1eefe93d56414f5f9e0b80b8f8412c
Pavel <pavelmalyv@gmail.com>
2025-04-15 15:08:56 +0300

    refactor(Logo): расширить пропсы от базового Link

--- src/components/UI/logo/Logo.tsx
+++ src/components/UI/logo/Logo.tsx
@@ -1,10 +1,13 @@
+import type { LinkProps } from 'react-router';
+
 import cl from './Logo.module.scss';
 import { Link } from 'react-router';
 import { baseUrl } from '@/routes/routes';
+import classNames from 'classnames';
 
-const Logo = () => {
+const Logo = ({ className, ...props }: Omit<LinkProps, 'to'>) => {
 	return (
-		<Link to={baseUrl} className={cl.logo}>
+		<Link to={baseUrl} className={classNames(cl.logo, className)} {...props}>
 			Blog
 		</Link>
 	);
